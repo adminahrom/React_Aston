e43f5469596199f16582171b5646109535d15c28
adminahrom <adminahrom@mail.ru>
2024-05-08 16:54:37 +0300

    lesson_1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import FormBtn from './components/FormBtn/FormBtn';
+import FormSearch from './components/FormSearch/FormSearch';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<FormBtn />
+			<FormSearch />
+		</div>
+	);
 }
 
 export default App;
